06099857370dfeef4995b200261bdcb9e1bb1ab6
moccbc <yutanak2010@gmail.com>
2020-06-16 10:13:03 -0700

    Added tests for splitter

--- unit_tests/splitter_tests/split_test.hpp
+++ unit_tests/splitter_tests/split_test.hpp
@@ -12,17 +12,39 @@ TEST(SplitterTest, SplitSingle)
 
 TEST(SplitterTest, SplitMultiple)
 {
-    std::string expression = "echo hello && echo world";
+    std::string expression = "echo hello && echo world ";
     Splitter* s = new Splitter(expression);
     std::vector<std::string> output = s->split();
 
-    std::string output_string = "";
+    EXPECT_EQ("echo", output[0]);
+    EXPECT_EQ("hello", output[1]);
+    EXPECT_EQ("&&", output[2]);
+    EXPECT_EQ("echo", output[3]);
+    EXPECT_EQ("world", output[4]);
+}
 
-    for (int i = 0; i < output.size(); i++) {
-        output_string += output[i];
-        output_string += ' ';
-    }
+TEST(SplitterTest, SplitOverLoad)
+{
+    std::string expression = "( echo hello && echo world ) ; ( echo good || echo bye ) && ls ";
+    Splitter* s = new Splitter(expression);
+    std::vector<std::string> output = s->split();
 
-    EXPECT_EQ("echo hello ", output_string);
+    EXPECT_EQ("(", output[0]);
+    EXPECT_EQ("echo", output[1]);
+    EXPECT_EQ("hello", output[2]);
+    EXPECT_EQ("&&", output[3]);
+    EXPECT_EQ("echo", output[4]);
+    EXPECT_EQ("world", output[5]);
+    EXPECT_EQ(")", output[6]);
+    EXPECT_EQ(";", output[7]);
+    EXPECT_EQ("(", output[8]);
+    EXPECT_EQ("echo", output[9]);
+    EXPECT_EQ("good", output[10]);
+    EXPECT_EQ("||", output[11]);
+    EXPECT_EQ("echo", output[12]);
+    EXPECT_EQ("bye", output[13]);
+    EXPECT_EQ(")", output[14]);
+    EXPECT_EQ("&&", output[15]);
+    EXPECT_EQ("ls", output[16]);
 }
 
